fb95d768bea0473adaa805e64aa15d6acf684565
Kiranjit-sudo <kiranjit.pattnaik99@hotmail.com>
2025-06-03 01:16:57 +0000

    os path add

--- streamlit_app.py
+++ streamlit_app.py
@@ -180,9 +180,10 @@ if st.button("Search Images"):
                     # st.image(load_indexed_image_paths[idx])
                     image_path="/testimages/" + df.iloc[idx]['filename']
                     st.markdown(image_path)
-                    files = os.listdir('testimages')
-                    for file in files:
-                        st.markdown(file)
+                    # files = os.listdir('testimages')
+                    # for file in files:
+                    #     st.markdown(file)
+                    st.markdown(os.path.join('testimages', df.iloc[idx]['filename'])
                     st.image(Image.open(image_path))
                     st.markdown(f"(Similarity: {distances[0][i]:.4f})")
                     st.markdown(f"Caption: {df.iloc[idx]['filename']}")
